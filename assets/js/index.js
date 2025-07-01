c93c12e5ff07f10e2aef75115525c318d5931f85
kamlesh914213 <143068135+kamlesh914213@users.noreply.github.com>
2025-07-02 03:41:41 +0530

    Updated team card styles with smooth hover effect

--- assets/js/index.js
+++ assets/js/index.js
@@ -63,6 +63,30 @@ $(document).ready(function () {
 });
 
 
+$(".service_crousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  navText: [
+    '<i class="fas fa-long-arrow-alt-left"></i>',
+    '<i class="fas fa-long-arrow-alt-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
